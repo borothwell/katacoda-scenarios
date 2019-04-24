b6f312a004003a2dffec74c023acaaa153ce0de3
borothwell <45627856+borothwell@users.noreply.github.com>
2019-04-23 21:39:08 -0700

    Update foreground.sh

--- interview2/foreground.sh
+++ interview2/foreground.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl https://raw.githubusercontent.com/DataDog/TrainingEnvironment/master/apm/install.sh | bash -s '685b979c09e8e4bfaac9a9515d937c3e'
+curl https://raw.githubusercontent.com/DataDog/TrainingEnvironment/master/apm/install.sh | bash -s 'db94d2d6ea10f9ca48da82080a957bd6'
